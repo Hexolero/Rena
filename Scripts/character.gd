6daaf42ae888a43a7e74148d5c573749874c871d
jonathan-keeler <15jwk1@queensu.ca>
2018-02-11 23:12:44 -0500

    Camera swap now working

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -8,14 +8,15 @@ onready var gridMap = get_node(gridMapPath)
 const ILYA_MOVESPEED = 6.0
 const RENA_MOVESPEED = 9.0
 
-const ILYA_CAMERAPOS = Vector3(0, 2.36, 0.65) # this is when camera container is a child of ilya
-const ILYA_CAMERA_ROT = PI
-
-const RENA_CAMERAPOS = Vector3(0, 0.92, 1.42) # this is when camera container is a child of rena
-const RENA_CAMERA_ROT = PI
+const ILYA_CAMERA_POS = Vector3(0, 2.36, 0.65) # this is when camera container is a child of ilya
+const RENA_CAMERA_POS = Vector3(0, 0.92, 1.42) # this is when camera container is a child of rena
 
 # Shared constants
 const MOUSE_CAMERA_TURN_SPEED = 0.005
+
+const CAMERA_MOVESPEED_T = 8.0
+const CAMERA_MOVESPEED_R = (PI / 16) * CAMERA_MOVESPEED_T
+
 const CAMERA_HIGH_ANGLE = -PI / 3
 const CAMERA_LOW_ANGLE = PI / 3
 
@@ -26,7 +27,24 @@ const SIGHTCAST_EXTENT = 1000
 
 # Character variables
 var currentCharacter = "Ilya"
+
 var characterSwapQueued = false
+var nextCharacter
+
+# swap variables
+var swapOriginPos
+var swapOriginRotation
+var swapDeltaPos
+var swapDeltaRotation
+
+var cameraTranslating
+var translateTimer
+var translationTime
+
+var cameraRotating
+var rotateTimer
+var rotationTime
+# end swap variables
 
 var moveArrAction = ["movement_up", "movement_left", "movement_down", "movement_right"]
 var moveArrCheck = [false, false, false, false]
@@ -46,16 +64,40 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
-	# check if player intends to swap characters
-	if characterSwapQueued:
+	# check if player intends to swap characters (and is currently able to)
+	var currentCharNode
+	if currentCharacter == "Ilya":
+		currentCharNode = $ilya
+	else:
+		currentCharNode = $rena
+	
+	if characterSwapQueued && currentCharNode.is_on_floor():
 		characterSwapQueued = false # consume event
 		if is_friend_in_sight():
-			print("CHARACTER_SWAP INITIATED")
-			$rena.rotate_y(PI / 2)
+			swap_character()
 	
 	if currentCharacter == "Swapping":
-		return # do nothing physics-wise with the characters while swapping
-	if currentCharacter == "Ilya":
+		if cameraTranslating:
+			$camera_container.translation += (delta / translationTime) * swapDeltaPos
+		if cameraRotating:
+			$camera_container.rotation += (delta / rotationTime) * swapDeltaRotation
+		
+		# Check for interpolation end
+		if !cameraTranslating && !cameraRotating:
+			var cc_ref = $camera_container
+			if nextCharacter == "Ilya":
+				remove_child(cc_ref)
+				$ilya.add_child(cc_ref)
+				cc_ref.translation = ILYA_CAMERA_POS
+				cc_ref.rotation = Vector3()
+			elif nextCharacter == "Rena":
+				remove_child(cc_ref)
+				$rena.add_child(cc_ref)
+				cc_ref.translation = RENA_CAMERA_POS
+				cc_ref.rotation = Vector3()
+			
+			currentCharacter = nextCharacter
+	elif currentCharacter == "Ilya":
 		# rotate camera
 		$ilya.rotate_y(-mouseFrameDelta.x * MOUSE_CAMERA_TURN_SPEED)
 		
@@ -84,7 +126,7 @@ func _physics_process(delta):
 		for i in range(4):
 			if moveArrCheck[i]:
 				movementDir += moveArrDir[i].rotated(UP, $ilya.rotation.y)
-		$ilya.move_and_slide(ILYA_MOVESPEED * movementDir)
+		$ilya.move_and_slide(ILYA_MOVESPEED * movementDir, UP)
 		
 		# apply gravity and update is_on_floor().
 		$ilya.move_and_slide(currentFallSpeed * UP, UP) # MAS automatically uses delta
@@ -111,9 +153,7 @@ func _input(event):
 	
 	# Mouse input
 	if event.is_action_pressed("swap_character"):
-		print("CHARACTER_SWAP ATTEMPT")
 		characterSwapQueued = true
-		
 	if event is InputEventMouseMotion:
 		mouseFrameDelta += event.relative
 
@@ -139,3 +179,76 @@ func is_friend_in_sight():
 		sightcast_data = space_state.intersect_ray(sightcast_origin, sightcast_origin + sightcast_normal * SIGHTCAST_EXTENT, exceptions)
 		if !sightcast_data.empty():
 			return sightcast_data.collider.name == "ilya"
+
+func swap_character():
+	# We can successsfully start a character swap.
+	
+	var swapNewPos
+	var swapNewRotation
+	var cc_ref # camera_container reference
+	
+	if currentCharacter == "Ilya":
+		# Save position + rotation of camera (These are global)
+		swapOriginPos = $ilya/camera_container.translation + $ilya.translation
+		swapOriginRotation = Vector3($ilya/camera_container.rotation.x, $ilya.rotation.y, 0.0)
+		# Set up new position + rotation of camera (These are global)
+		swapNewPos = RENA_CAMERA_POS + $rena.translation
+		swapNewRotation = Vector3(0.0, $rena.rotation.y, 0.0)
+		# Set cc_ref
+		cc_ref = $ilya/camera_container
+	elif currentCharacter == "Rena":
+		# Save position + rotation of camera (These are global)
+		swapOriginPos = $rena/camera_container.translation + $rena.translation
+		swapOriginRotation = Vector3($rena/camera_container.rotation.x, $rena.rotation.y, 0.0)
+		# Set up new position + rotation of camera (These are global)
+		swapNewPos = ILYA_CAMERA_POS + $ilya.translation
+		swapNewRotation = Vector3(0.0, $ilya.rotation.y, 0.0)
+		# Set cc_ref
+		cc_ref = $rena/camera_container
+	
+	# Calculate global deltas
+	swapDeltaPos = swapNewPos - swapOriginPos
+	swapDeltaRotation = swapNewRotation - swapOriginRotation
+	
+	# Calculate times + Timer setup
+	translationTime = swapDeltaPos.length() / CAMERA_MOVESPEED_T
+	cameraTranslating = true
+	translateTimer = Timer.new()
+	add_child(translateTimer)
+	translateTimer.connect("timeout", self, "stop_translating")
+	translateTimer.wait_time = translationTime
+	translateTimer.one_shot = true
+	translateTimer.start()
+	
+	rotationTime = swapDeltaRotation.length() / CAMERA_MOVESPEED_R
+	cameraRotating = true
+	rotateTimer = Timer.new()
+	add_child(rotateTimer)
+	rotateTimer.connect("timeout", self, "stop_rotating")
+	rotateTimer.wait_time = rotationTime
+	rotateTimer.one_shot = true
+	rotateTimer.start()
+	
+	# Unparent camera_container and set its position/rotation properly
+	if currentCharacter == "Ilya":
+		$ilya.remove_child(cc_ref)
+		add_child(cc_ref)
+		
+		nextCharacter = "Rena"
+	elif currentCharacter == "Rena":
+		$rena.remove_child(cc_ref)
+		add_child(cc_ref)
+		
+		nextCharacter = "Ilya"
+	
+	cc_ref.translation = swapOriginPos
+	cc_ref.rotation = swapOriginRotation
+	
+	# Finally, set currentCharacter to the swapping state
+	currentCharacter = "Swapping"
+
+func stop_translating():
+	cameraTranslating = false
+
+func stop_rotating():
+	cameraRotating = false
